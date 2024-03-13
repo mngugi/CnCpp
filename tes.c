ed3d9ad3d92abf2a0b5ec2da10db9f4c4ab1ca96
M.ngugi <mngugi7@gmail.com>
2024-03-13 06:44:08 -0400

    tes

---
+++ tes.c
@@ -0,0 +1,18 @@
+/****************************************************************
+* Author : Mwangi Ngugi
+* Purpose : a simple hello world using a function
+*
+*Function to me is the beging of coding , however learning basics
+* is important
+*****************************************************************/
+#include<stdio.h>
+void start()
+{
+	printf("Cblocks won't open");
+}
+int main()
+{
+   // calls the start function
+   start();
+   return 0;
+}
