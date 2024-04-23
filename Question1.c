b872ab408c9efee377db5510decd72b5a17a4b69
M.ngugi <mngugi7@gmail.com>
2024-04-23 23:00:42 +0300

    Question1

--- Question1.c
+++ Question1.c
@@ -6,15 +6,15 @@ int main() {
 
 int a = 15;
 int b = 24;
-printf("Memory address of variable a: %p\n", (void *)&a);
-printf("\nMemory address of variable a: %p\n", (void *)&b);
+printf("\nMemory address of variable a: %p\n", (void *)&a);
+printf("Memory address of variable a: %p\n", (void *)&b);
 
 
-printf("%d %d\n", b-a+7, b-(a+7));
-
+printf("\n%d %d\n", b-a+7, b-(a+7));
 printf("%d %d\n", b-a-4, b-(a-4));
 printf("%d %d\n", b%a/2, b%(a/2));
 printf("%d %d\n", b*a/2, b*(a/2));
 printf("%d %d\n", b/2*a, b/(a*2));
 
+return EXIT_SUCCESS;
 }
