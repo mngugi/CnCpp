c12ec66cfa383c82c26ee7e227e7d54512e9e049
M.ngugi <mngugi7@gmail.com>
2024-04-24 21:33:50 +0300

    read_string.c

---
+++ read_string.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+char item[50];
+
+printf("Enter Item Name: ");
+fgets(item, sizeof(item), stdin);
+printf("Item, %s\n", item);
+
+return EXIT_SUCCESS;
+
+
+
+
+}
--- scanfRead.c
+++ scanfRead.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
 * prompt a number
 * enter number
