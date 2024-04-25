167c8ed1fcf4cf8c2c2363a38b368e26e53117d8
M.ngugi <mngugi7@gmail.com>
2024-04-25 22:08:20 +0300

    area0fTriangle

--- area0fTriangle.c
+++ area0fTriangle.c
@@ -7,10 +7,21 @@ int base, height;
 double area;
 
 printf("\nEnter the base: ");
-scanf("%d",&base);
+if (scanf("%d",&base) !=1) {
+printf("Error: Invalid input for base!");
+return EXIT_FAILURE;
+}
 
 printf("\nEnter height: ");
-scanf("%d", &height);
+if (scanf("%d", &height) !=1){
+printf("Error: Invalid input for base!");
+return EXIT_FAILURE;
+}
+
+if (base <= 0 || height <= 0) {
+        printf("Error: Base and height must be positive.\n");
+        return EXIT_FAILURE;
+    }
 
 area = (height * base) / 2.0;
 printf("\nThe Area is %.2f\n: ", area);
