38fb089c2f7943a46078dbd6052002587ccea648
M.ngugi <mngugi7@gmail.com>
2024-04-25 21:24:45 +0300

    areaofTriangle

---
+++ area0fTriangle.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+int base, height, area;
+double C = 1/2;
+
+printf("\nEnter the base: ");
+scanf("%d",&base);
+
+printf("\nEnter height: ");
+scanf("%d", &height);
+
+printf("\n Area: ");
+scanf("%d * %d * %f", base, height, C );
+
+
+return 0;
+
+
+
+
+
+}
