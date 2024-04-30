eaa869d3ed48838a0b6eaa760164060d8e36cedd
M.ngugi <mngugi7@gmail.com>
2024-05-01 00:20:05 +0300

    twoNumbers

--- twoNumbers.c
+++ twoNumbers.c
@@ -1,38 +1,37 @@
 //prompt for two numbers and find their sum
 #include <stdio.h>
+
 int main() {
-int a, b, c, d, squared;
+    int a, b, c, d, squared;
+    double avg;
 
-double avg;
-printf("\nMemory address of variable a: %p\n", (void *)&a);
-printf("Memory address of variable b: %p\n", (void *)&b);
-printf("Memory address of variable c: %p\n", (void *)&c);
-printf("Memory address of variable avg: %p\n", (void *)&avg);
-printf("Memory address of variable squared: %p\n", (void *)&squared);
+    printf("Enter the first number: ");
+    scanf("%d", &a);
 
-printf("Enter first number: ");
-scanf("%d", &a);
-printf("Enter second number: ");
-scanf("%d", &b);
+    printf("Enter the second number: ");
+    scanf("%d", &b);
 
-printf("Enter second number: ");
-scanf("%d", &c);
+    printf("Enter the third number: ");
+    scanf("%d", &c);
 
-printf("%d + %d + %d = %d\n", a, b, c, a+b+c);
+    printf("Memory address of variable a: %p\n", (void *)&a);
+    printf("Memory address of variable b: %p\n", (void *)&b);
+    printf("Memory address of variable c: %p\n", (void *)&c);
 
-printf("Enter 3 integers: ");
-scanf("%d %d %d", &a,&b,&c);
+    printf("%d + %d + %d = %d\n", a, b, c, a + b + c);
 
-avg = (a+b+c) / 3.0;
-printf("\n The average is %3.1f\n", avg);
+    avg = (a + b + c) / 3.0;
+    printf("The average is %.1f\n", avg);
 
-printf("\nEnter the Square number: ");
-scanf("%d ", &d);
+    printf("Enter the number to square: ");
+    scanf("%d", &d);
 
-squared = d*d;
-printf("\n The Square of %d is %d\n :", d, squared ); 
+    printf("Memory address of variable d: %p\n", (void *)&d);
 
-return EXIT_SUCCESS;
+    squared = d * d;
+    printf("The square of %d is %d\n", d, squared);
 
+    printf("Memory address of variable squared: %p\n", (void *)&squared);
 
+    return 0;
 }
