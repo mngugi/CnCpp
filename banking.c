c062c99e26a50f8fa7834a32e056ac5e4d1adb7d
M.ngugi <mngugi7@gmail.com>
2024-05-02 21:43:15 +0300

    banking

--- banking.c
+++ banking.c
@@ -1,21 +1,33 @@
-//calculate interest and service charge for bank customer
 #include <stdio.h>
+
 int main() {
-char customer[30], acctNum[30];
-double avgBalance, interest, service;
-int numTrans;
-printf("Name? ");
-gets(customer);
-printf("Account number? ");
-gets(acctNum);
-printf("Average balance? ");
-scanf("%lf", &avgBalance);
-printf("Number of transactions? ");
-scanf("%d", &numTrans);
-interest = avgBalance * 0.06;
-service = numTrans * 0.50;
-printf("\nName: %s\n", customer);
-printf("Average balance: $%3.2f\n", avgBalance);
-printf("Interest: $%3.2f\n", interest);
-printf("Service charge: $%3.2f\n", service);
+    char customer[30], acctNum[30];
+    double avgBalance, interest, service;
+    int numTrans;
+
+    // Input
+    printf("Name? ");
+    fgets(customer, sizeof(customer), stdin);
+
+    printf("Account number? ");
+    fgets(acctNum, sizeof(acctNum), stdin);
+
+    printf("Average balance? ");
+    scanf("%lf", &avgBalance);
+
+    printf("Number of transactions? ");
+    scanf("%d", &numTrans);
+
+    // Calculate interest and service charges
+    interest = avgBalance * 0.06;
+    service = numTrans * 0.50;
+
+    // Output
+    printf("\nName: %s", customer);
+    printf("Account number: %s", acctNum);
+    printf("Average balance: $%.2f\n", avgBalance);
+    printf("Interest: $%.2f\n", interest);
+    printf("Service charge: $%.2f\n", service);
+
+    return 0;
 }
