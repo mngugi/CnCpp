4bbe1da98f1651752842090ddf7225ac8a22a6a7
M.ngugi <mngugi7@gmail.com>
2024-05-18 22:08:03 +0300

    Create readCustomerData.c

---
+++ readCustomerData.c
@@ -0,0 +1,12 @@
+/**Write a program to read the data for the customer, calculate the interest and service charge,
+and print the customer’s name, average balance, interest, and service charge.
+The following is a sample run of the program:
+Name? Alice Wonder
+Account number? 4901119250056048
+Average balance? 2500
+Number of transactions? 13
+Name: Alice Wonder
+Average balance: $2500.00
+Interest: $150.00
+Service charge: $6.50
+ **/
