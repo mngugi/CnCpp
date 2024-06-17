3d2be2ddce3a71c4ec18ff71215f6aba1506a816
M.ngugi <mngugi7@gmail.com>
2024-06-18 00:04:00 +0300

    areaSquare

--- areaSquare.c
+++ areaSquare.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
 
 int a, s ;
+
+int i,j,k;
+
+
+double average;
 
 printf("Enter side length: ");
 scanf("%d", &s);
@@ -11,7 +17,15 @@ a = s * s ;
 
 printf("Area  of the square is: %d\n", a);
 
+printf("-----------------------------\n");
 
+printf("\nEnter 3 integers: ");
+scanf("%d %d %d", &i, &j, &k);
 
+average = (i + j + k)/3.0;
+
+printf("/n The average is %3.1f\n", average);
+
+return EXIT_SUCCESS;
 
 }
