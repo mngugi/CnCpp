fb62bc0db5e7c4d66ca0158fb3de426e794e0712
M.ngugi <mngugi7@gmail.com>
2024-07-10 14:22:59 +0300

    check_pipewire

--- pipe_wire.c
+++ pipe_wire.c
@@ -2,9 +2,11 @@
 #include <stdio.h>
 
 int main() {
+    printf("Initializing PipeWire...\n");
     // Initialize PipeWire
     pw_init(NULL, NULL);
 
+    printf("Creating PipeWire main loop...\n");
     // Create a PipeWire main loop
     struct pw_main_loop *main_loop = pw_main_loop_new(NULL);
     if (main_loop == NULL) {
@@ -13,6 +15,7 @@ int main() {
         return 1;
     }
 
+    printf("Creating PipeWire context...\n");
     // Create a PipeWire context
     struct pw_context *context = pw_context_new(pw_main_loop_get_loop(main_loop), NULL, 0);
     if (context == NULL) {
@@ -22,6 +25,7 @@ int main() {
         return 1;
     }
 
+    printf("Connecting to PipeWire core...\n");
     // Create a PipeWire core proxy
     struct pw_core *core = pw_context_connect(context, NULL, 0);
     if (core == NULL) {
@@ -34,12 +38,23 @@ int main() {
 
     printf("PipeWire is running.\n");
 
-    // Clean up
+    // Disconnecting from PipeWire core
+    printf("Disconnecting from PipeWire core...\n");
     pw_core_disconnect(core);
+
+    // Destroying PipeWire context
+    printf("Destroying PipeWire context...\n");
     pw_context_destroy(context);
+
+    // Destroying PipeWire main loop
+    printf("Destroying PipeWire main loop...\n");
     pw_main_loop_destroy(main_loop);
+
+    // Deinitializing PipeWire
+    printf("Deinitializing PipeWire...\n");
     pw_deinit();
 
+    printf("Program finished.\n");
     return 0;
 }
 
