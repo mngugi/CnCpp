e7a8ff2370b348cfac44e1cdda4806885f77bce9
M.ngugi <mngugi7@gmail.com>
2024-07-11 13:32:50 +0300

    gcdAlgorithm

---
+++ gcdAlgorithm.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+// Function to calculate GCD using Euclidean algorithm
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main() {
+    int num1, num2;
+
+    // Input two numbers
+    printf("Enter two integers: ");
+    scanf("%d %d", &num1, &num2);
+
+    // Calculate GCD
+    int result = gcd(num1, num2);
+
+    // Output the result
+    printf("GCD of %d and %d is %d\n", num1, num2, result);
+
+    return 0;
+}
+
