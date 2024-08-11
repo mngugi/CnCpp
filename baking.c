a33867087dc039e336f9ab67521c0a34e70bf1e1
M.ngugi <mngugi7@gmail.com>
2024-08-11 21:14:54 +0300

    Create baking.c

---
+++ baking.c
@@ -0,0 +1,11 @@
+/*****
+Baking :
+How much butter is left in a 10-pound tub if 2 2/3
+are used for a wedding cake?
+
+Analyze
+- The contained 10 pounds of butter.
+- 2 2/3 of butter are used for cake.
+- How much butter is left in the tub?
+
+******/
