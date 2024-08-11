4cf73ce85fecae7fb1deb28054e1cf6c3e602abd
M.ngugi <mngugi7@gmail.com>
2024-08-11 21:22:56 +0300

    Update baking.c

--- baking.c
+++ baking.c
@@ -1,11 +1,11 @@
 /*****
 Baking :
-How much butter is left in a 10-pound tub if 2 2/3
+How much butter is left in a 10-pound tub if 2(2/3)
 are used for a wedding cake?
 
 Analyze
-- The contained 10 pounds of butter.
-- 2 2/3 of butter are used for cake.
+- The tub contained 10 pounds of butter.
+- 2(2/3) of butter are used for a cake.
 - How much butter is left in the tub?
 
 ******/
