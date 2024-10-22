7825d5777fd6dcbc236ae301da9d270a4cbb7fdf
mngugi <mngugi7@gmail.com>
2024-10-22 23:13:02 +0300

    Update cRecap.c

--- cRecap.c
+++ cRecap.c
@@ -3,10 +3,17 @@
 int main() {
     // create an int
     int num = 12;
-    float number = 63.21;
+
     printf("%d", num,"\n");
     printf(\n);
+    float number = 63.21;
     printf("f", number);
+    // create a char variable
+    char alphabet = 's';
+
+    // to print char variable, we use %c
+    printf("%c", alphabet);
+
 
     // create a float type variable
 
