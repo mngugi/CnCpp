c817128aeac8d5bec9198db5d4b20e814c958b68
mngugi <mngugi7@gmail.com>
2024-10-31 20:03:52 +0300

    Create engeering1.c

---
+++ engeering1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int result;
+    int a = 74, b = 98 ;
+
+
+    asm ("addl %%ebx, %%eax" : "=a" (result) : "a" (a), "b", result);
+
+    printf("Result is : %d\n", result);
+
+}
