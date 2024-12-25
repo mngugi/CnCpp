e9f3c4c266aaac191430e309385644c0331f409d
M.Ngugi <mngugi7@gmail.com>
2024-12-26 00:17:08 +0300

    Update arrayInsertion.cpp

--- arrayInsertion.cpp
+++ arrayInsertion.cpp
@@ -6,15 +6,24 @@ int main() {
     int insertIndex = 3;
 // print the original array
     for (int i = 0; i < sizeof(arr) / (arr[0]); i++ ) {
-    std::cout << arr[i] << std::endl;
+        std::cout << arr[i] << std::endl;
 
     }
     std::cout << "\n";
 
 // create a new array
     int newArraySize = sizeof(arr) / sizeof(arr[0]) + 1;
-    int newArr[newArraySize]
+        int newArr[newArraySize]
 
+// copy elements from old array to new array , leaving space for new element
+    for (int i = insertIndex + 1 ; i < newArraySize; i++)
+        newArr[i] = array[i - 1];
+
+    // print new array
+    std::cout << "Array after insertion:\n";
+    for (int i = 0; i < newArraySize; i++) {
+        std::cout << newArray[i] << " ";
+    }
 
     return 0;
 
