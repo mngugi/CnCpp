5c41e3ebe271af55da560cde352593b22e14e82a
M.Ngugi <mngugi7@gmail.com>
2024-12-25 13:21:52 +0300

    Update arrayInsertion.cpp

--- arrayInsertion.cpp
+++ arrayInsertion.cpp
@@ -4,11 +4,17 @@ int main() {
     int arr[] = {1,2,3,4,5};
     int insertVal = 20;
     int insertIndex = 3;
-
+// print the original array
     for (int i = 0; i < sizeof(arr) / (arr[0]); i++ ) {
     std::cout << arr[i] << std::endl;
 
     }
+    std::cout << "\n";
+
+// create a new array
+    int newArraySize = sizeof(arr) / sizeof(arr[0]) + 1;
+    int newArr[newArraySize]
+
 
     return 0;
 
