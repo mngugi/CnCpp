d1992ffd3a76724ecb9d3831a406417dcd41a0ac
M.Ngugi <mngugi7@gmail.com>
2024-12-25 13:14:40 +0300

    Update arrayInsertion.cpp

--- arrayInsertion.cpp
+++ arrayInsertion.cpp
@@ -2,6 +2,8 @@
 
 int main() {
     int arr[] = {1,2,3,4,5};
+    int insertVal = 20;
+    int insertIndex = 3;
 
     for (int i = 0; i < sizeof(arr) / (arr[0]); i++ ) {
     std::cout << arr[i] << std::endl;
