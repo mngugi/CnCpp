95e30da40024fbdc623b5fddfcd57d1b1dd260ba
M.Ngugi <mngugi7@gmail.com>
2024-12-25 12:53:57 +0300

    Update arrayTraversal.cpp

--- arrayTraversal.cpp
+++ arrayTraversal.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 
 int main() {
-    int arr[] = {0,1,2,3,4,5};
+    int arr[] = {1,2,3,4,5};
 
-    for (int i = 0; i < sizeof(arr)/ (arr[0]); i++ )
+    for (int i = 0; i < sizeof(arr)/ (arr[0]); i++ ) {
     std::cout << arr[i] << std::endl;
 
-}
+    }
+
+    return 0;
+
+ }
+
