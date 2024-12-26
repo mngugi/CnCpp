6681fc34bc91cde3ec050545462351f4617c331f
M.Ngugi <mngugi7@gmail.com>
2024-12-26 12:51:06 +0300

    pattern1.c

---
+++ pattern1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    printf("A\n");
+    printf("A B\n");
+    printf("A B C \n");
+    printf("A B C D \n");
+    printf("A B C D E \n");
+
+    return 0;
+
+
+}
