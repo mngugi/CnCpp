5b79e43f830e7d229b92cbc92542794f32d95c72
mngugi <mngugi7@gmail.com>
2025-03-26 21:42:03 +0300

    Create arrayTraversal2.cpp

---
+++ arrayTraversal2.cpp
@@ -0,0 +1,8 @@
+
+int main() {
+    int array_two[] = {7,8,9,6,5};
+    for (int i=0; i < sizeof(array_two)/array_two[0]; i++ ) {
+        std::cout << array_two << std:: endl;
+    }
+    return 0;
+}
