8a106aef45b4dae73f1d07facc2f45cf3b3fd987
mngugi <mngugi7@gmail.com>
2025-03-28 20:12:43 +0300

    Update cppConstants.cpp

--- cppConstants.cpp
+++ cppConstants.cpp
@@ -6,5 +6,20 @@
 
 int main(){
 
-    
+    // initialize a constant
+
+    const double PI = 3.141;
+
+    // create a read-only reference to PI
+    const double &PI_REF = PI;
+
+    cout << "PI" << PI;
+    cout << "\nPI_REF: " << PI_REF;
+
+
+    return 0 ; 
+
+
+
+
 }
