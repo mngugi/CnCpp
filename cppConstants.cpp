b1370967e75699cbef487a47e30123b552365a41
mngugi <mngugi7@gmail.com>
2025-03-28 20:02:25 +0300

    Update cppConstants.cpp

---
+++ cppConstants.cpp
@@ -0,0 +1,10 @@
+/**
+ * The const keyword (which stands for constant) is used to specify that the value of a variable
+ *  cannot be changed anywhere in the program, either intentionally or accidentally.
+ */
+#include <iostream>
+
+int main(){
+
+    
+}
