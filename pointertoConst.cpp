c12c46bdc6b52ed4b15a0aabadbbf43f05b37f31
mngugi <mngugi7@gmail.com>
2025-03-30 22:23:16 +0300

    Update pointertoConst.cpp

--- pointertoConst.cpp
+++ pointertoConst.cpp
@@ -10,8 +10,8 @@ int main()
     // MONTHS_PTR is a pointer to int const
     /**
      * A pointer to a const is a pointer variable that points to a constant variable. These pointers
-     *allow us to change the address the pointer is pointing to,
-     *don't allow us to change the value stored in those constant variables.
+     * allow us to change the address the pointer is pointing to,
+     * don't allow us to change the value stored in those constant variables.
      */ 
      const int *MONTHS_PTR = &NUMBER_MONTHS ; 
 
