fbe2d67e2f787eb88f73aa123b28c7ab646976b8
mngugi <mngugi7@gmail.com>
2025-03-31 20:50:58 +0300

    Update constPointer.cpp

---
+++ constPointer.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+
+    string fruit1 = "Applr";
+    string fruit2 = "Orange";
+
+    cout << "Initially, fruit1: " << fruit1 << endl;
+
+    
+
+}
