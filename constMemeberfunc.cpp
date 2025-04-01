cdc3aa9895170f046a668b908f114cd93e12e376
mngugi <mngugi7@gmail.com>
2025-04-01 15:30:46 +0300

    Update constMemeberfunc.cpp

---
+++ constMemeberfunc.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace  std;
+
+class Rectangle {
+private:
+    int breadth, length;
+public:
+    Rectangle(int length, int breadth){
+        this->breadth = breadth;
+        this->length = length;
+
+
+    }
+};
+
+
+    
