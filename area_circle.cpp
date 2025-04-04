35b2d347df8e4446eefbe1f31d5d230227b72dd3
mngugi <mngugi7@gmail.com>
2025-04-04 13:23:22 +0300

    Update area_circle.cpp

--- area_circle.cpp
+++ area_circle.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
 using namespace std;
-
+/**
+ * make a class for a circle
+ * class circle represents a geeometric circle. 
+ */
 class Circle
 {
 private:
     /* data */
+    /*Constant value of PI used in area calculation.*/
     const double PI = 3.141592653589793;
-    int radius; 
+    int radius; /*radius of a circle*/
 
 public:
+
     Circle(int radius){
         this->radius=radius ;
 
     }
+
+    /**
+     * Calculates and returns the area of the circle.
+     * 
+     * The area of the circle as a double.
+     */
 
 int get_area() const {
     return PI * radius * radius ;
