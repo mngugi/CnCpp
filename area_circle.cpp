153590b1c2f62e489ff25d755d11a4c5e2184cb9
mngugi <mngugi7@gmail.com>
2025-04-04 12:47:22 +0300

    Update area_circle.cpp

---
+++ area_circle.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+class area_circle
+{
+private:
+    /* data */
+    const double PI = 3.141592653589793;
+    int radius; 
+public:
+    area_circle(/* args */);
+    ~area_circle();
+};
+
+area_circle::area_circle(/* args */)
+{
+}
+
+area_circle::~area_circle()
+{
+}
+
