c940f05e554bd23a156c36113fcfdf7da0cd1c58
mngugi <mngugi7@gmail.com>
2025-04-04 14:26:04 +0300

    Update isStatements.cpp

--- isStatements.cpp
+++ isStatements.cpp
@@ -6,8 +6,12 @@ int main(){
     cout << "Enter exam marks: ";
     cin >> Exam_marks ; 
 
-    
+    // check number is positive
 
+    if (Exam_marks >= 85) {
+        cout << "Grade High Distinction: " <<  Exam_marks << endl;
+    }
 
+  return 0; 
 }
 
