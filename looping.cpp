06c4e40cf7a896521b33a2f23ec3f48cba57d5ec
mngugi <mngugi7@gmail.com>
2025-04-06 17:57:16 +0300

    Update looping.cpp

--- looping.cpp
+++ looping.cpp
@@ -5,3 +5,16 @@
 * update - updates the value of initialized variables and again checks the condition
  * 
  */
+
+ #include <iostream>
+
+using namespace std;
+
+int main(){
+    for(int j = 1 ; j <=10; ++j)
+    cout << j << " " << endl ;
+
+    return 0;
+
+
+}
