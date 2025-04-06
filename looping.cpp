3eabd2bf32b4d320b6fea5d9a6ef017f5a82b85c
mngugi <mngugi7@gmail.com>
2025-04-06 17:50:35 +0300

    Create looping.cpp

---
+++ looping.cpp
@@ -0,0 +1,7 @@
+/**
+* initialization - initializes variables and is executed only once
+* condition - if true, the body of for loop is executed
+* if false, the for loop is terminated
+* update - updates the value of initialized variables and again checks the condition
+ * 
+ */
