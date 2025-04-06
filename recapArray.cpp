465bd79f95abe3ed7f4d02e081e55bb9c374514e
mngugi <mngugi7@gmail.com>
2025-04-06 18:19:28 +0300

    Update recapArray.cpp

---
+++ recapArray.cpp
@@ -0,0 +1,13 @@
+/*
+* program that declared and initialized an int array named fruits_array. It has 10 items.
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    fruits_array[] = {"Apple", "Avocado", "Banana","Blackberry","Blueberry", "Dragon Fruit", "Orange", "Popo", "Pineapple", "strawberry"};
+    
+}
