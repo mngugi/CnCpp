87b50bbe60f577183257e07b82279de7807ebca9
mngugi <mngugi7@gmail.com>
2025-04-11 00:17:53 +0300

    Update whileloop.cpp

--- whileloop.cpp
+++ whileloop.cpp
@@ -1,11 +1,27 @@
 /**
  * A while loop evaluates the condition
- *If the condition evaluates to true, the code inside the while loop is executed.
- *The condition is evaluated again.
- *This process continues until the condition is false.
+ * If the condition evaluates to true, the code inside the while loop is executed.
+ * The condition is evaluated again.
+ * This process continues until the condition is false.
  * When the condition evaluates to false, the loop terminates.
-
+ * 
  * 
  */
 
- 
+
+#include <iostream>
+
+using namespace std;
+
+int main() {|
+    int i = 1;
+
+    while (i <= 10) {
+        cout << i << " ";
+        ++i;
+    }
+    
+    
+    return 0;
+}
+
