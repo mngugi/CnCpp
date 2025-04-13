bb45273a6f58e3d18f1cc15a926f5a8f44d60602
mngugi <mngugi7@gmail.com>
2025-04-13 16:48:24 +0300

    whileloop.cpp

--- whileloop.cpp
+++ whileloop.cpp
@@ -9,20 +9,38 @@
  */
 
 
-#include <iostream>
+ #include <iostream>
 
-using namespace std;
-
-int main() {
-    int i = 1;
-
-    while (i <= 10) {
-        cout << i << " " << endl;
-        ++i;
-    }
-    
-        cout << "==============" << endl; 
-    
-    return 0;
-}
+ using namespace std;
+ 
+ int main() {
+     int i = 1;
+     int j = 0;
+     int sum = 0;
+ 
+     // Print numbers 1 to 10
+     while (i <= 10) {
+         cout << i << endl;
+         ++i;
+     }
+ 
+     cout << "==============" << endl;
+ 
+     // Prompt user to enter numbers until a negative one is entered
+     cout << "Enter numbers to sum (negative number to stop):" << endl;
+ 
+     while (true) {
+         cout << "Enter number: ";
+         cin >> j;
+ 
+         if (j < 0) break;
+ 
+         sum += j;
+     }
+ 
+     cout << "\nThe sum is: " << sum << endl;
+ 
+     return 0;
+ }
+ 
 
