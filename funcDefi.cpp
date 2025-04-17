9c97f2047930e18ad1d4da93eee3b8e56fa9cb49
mngugi <mngugi7@gmail.com>
2025-04-17 12:03:22 +0300

    Update funcDefi.cpp

--- funcDefi.cpp
+++ funcDefi.cpp
@@ -1,5 +1,25 @@
 /**
  * Functions has two parts, 1.header and 2.the body.
- * Functions start with capital letter.alignas
+ * Functions start with capital letter.alignas.alignas
+ * Function body should be enclosed within curly braces {} and
+ * indented as a best practice to show association.
  * 
  */
+
+ #include <iostream>
+ using namespace std;
+
+ /**
+  * * void - Determines whether there is a return value or not for the function.
+  * If the function has no return value, use void. If the function returns an
+  * integer, use int, etc
+  */
+
+  void echoSomething() {
+
+    cout << "Global Warming!" << endl;
+    cout << "Global Warming!" << endl;
+
+
+
+  }
