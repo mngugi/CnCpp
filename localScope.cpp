460cddfa7ddc72deb9b97db971728f9a85a76fa1
mngugi <mngugi7@gmail.com>
2025-04-17 20:41:00 +0300

    Update localScope.cpp

---
+++ localScope.cpp
@@ -0,0 +1,20 @@
+/**
+ * 
+ * 
+ * 
+ */
+
+ #include <iostream>
+ using namespace std;
+
+ void func1(){
+    string your_name = "Bruno Fernandez";
+    cout << your_name << endl;
+
+ }
+
+ void func2(){
+    cout << your_name << endl;
+ }
+
+ 
