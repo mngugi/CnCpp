bc19182d0db02a0fb893848f94cdd5e219718070
mngugi <mngugi7@gmail.com>
2025-04-17 20:56:55 +0300

    Update localScope.cpp

--- localScope.cpp
+++ localScope.cpp
@@ -4,10 +4,22 @@
  * 
  * 
  */
+/**
+  * Global scope  - Referencing
+  * Global scope variable is when a variable 
+  * is declared out side all existing functions.
+  * 
+  */
 
  #include <iostream>
  using namespace std;
 
+ 
+string fruits = "Healthy Foods"
+
+void foods(){
+    cout << fruits << endl;
+  }
 
  void func1(){
     string your_name = "Bruno Fernandez";
@@ -20,22 +32,10 @@
     cout << your_name << endl;
  }
 
- /**
-  * Global scope  - Referencing
-  * Global scope variable is when a variable 
-  * is declared out side all existing functions.
-  * 
-  */
-
-  string fruits = "Healthy Foods"
-
-  void foods(){
-    cout << fruits << endl;
-  }
-
  int main(){
     func1();
     func2();
+    cout << '---------------' << endl;
     foods();
 
     return 0;
