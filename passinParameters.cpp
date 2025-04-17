4195f2d6b883cb29a6012343aa7270962a79d9b9
mngugi <mngugi7@gmail.com>
2025-04-17 14:06:22 +0300

    Create passinParameters.cpp

---
+++ passinParameters.cpp
@@ -0,0 +1,24 @@
+/*
+*
+* If a function contains parameters within its definition, they are required to
+* be present when the function is called.
+* The example below creates a function for adding and division. 
+*
+*/
+
+#include <iostream> 
+using namespace std;
+
+/**
+ *  the function,
+ *   Add(), adds two integer parameters together. Parameters are the types or
+ *   values located in between the parentheses. Multiple parameters are separated by commas.
+ * 
+ **/
+
+void add( int integerJ, int integerI, int integerK) {
+    cout << integerI + integerJ << endl ; 
+
+
+
+}
