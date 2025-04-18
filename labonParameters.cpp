e70cc4e15f400c26b81ef1be5b624643e091480c
mngugi <mngugi7@gmail.com>
2025-04-18 20:19:16 +0300

    Update labonParameters.cpp

--- labonParameters.cpp
+++ labonParameters.cpp
@@ -20,7 +20,22 @@ void  GetSlope( double x1, double x2, double y1, double y2) {
 
 int main() {
 
-    
+    cout << "Enter the first x1 coordinate:" << endl;
+    cin >> x1; 
+
+    cout << "Enter the first x2 coordinate:" << endl;
+    cin >> x2; 
+
+    cout << "Enter the first y1 coordinate:" << endl;
+    cin >> y1; 
+
+    cout << "Enter the first y2 coordinate:" << endl;
+    cin >> y2; 
+
+    // call the function to call get the slope.
+    GetSlope(x1,x2,y1,y2);
+
+ 
 }
 
 
