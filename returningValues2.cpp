8c1f93ae155bb3d82fcc62d4522285820597cf94
mngugi <mngugi7@gmail.com>
2025-04-18 13:04:53 +0300

    Update returningValues2.cpp

--- returningValues2.cpp
+++ returningValues2.cpp
@@ -1,6 +1,15 @@
 
 /**
  * Functions to return ints, doubles, strings,and vectors. 
- * 
+ * parameter i as the first integer
+ * parameter j as th second integer
+ * return sum(i,j)
  * 
  */
+
+ #include <iostream>
+ using namespace std;
+
+ int Returnsum( int i, int j){
+    return(i+j);
+ }
