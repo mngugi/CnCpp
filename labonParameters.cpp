adcceea7cc5e741b81abed672f5b9af3ad182ee9
mngugi <mngugi7@gmail.com>
2025-04-18 20:10:00 +0300

    Update labonParameters.cpp

---
+++ labonParameters.cpp
@@ -0,0 +1,20 @@
+
+/**
+* a function that takes in 4 doubles as parameters.
+* These parameters represent two sets of coordinate points labeled as x1, y1,
+* x2, and y2. The function will then use these points to calculate the slope that
+* their line creates and then prints that slope to the user.
+* 
+*/
+
+#include <iostream>
+using namespace std;
+
+void  GetSlop( double x1, double x2, double y1, double y2) {
+    doubleSlope = (y2 - y1) / (x2 - x1) ;
+    cout << "the Slope is " << endl;
+
+
+}
+
+
