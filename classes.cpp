8dc9442aa108246379ace6cf97f50d0b364e5855
mngugi <mngugi7@gmail.com>
2025-04-19 17:00:24 +0300

    Update classes.cpp

--- classes.cpp
+++ classes.cpp
@@ -8,7 +8,8 @@
  * Objects - Objects are constructed according to the blueprint that is the class.
  * Instance - Another way that programmers talk about objects is to say that
  * an object is an instance of a particular class. For example, s is an instance
- * of the string class.
+ * of the string class. [string s = "this is a string";
+ * s.length()
  * Instantiation - Instantiation is the process where an object is created
  * according to a blueprint of the class. The phrase “define a variable” means
  * to create a variable. The variable is given a name and a value. Once it has
@@ -21,4 +22,36 @@
  #include<string>
 
  using namespace std;
+    // class definition 
 
+    class Actor
+    {
+   
+    public:
+         string first_name;
+         string last_name;
+         string birth_date;
+         int age;
+         int total_movies;
+         int oscar_wins;
+         int oscar_naminations;
+
+    Actor () {
+
+        first_name = "Denzel"
+        last_name = "Washington";
+        birth_date = "December 28, 1954";
+        age = 68;
+        total_movies= 50;
+        oscar_wins = 2;
+        oscar_naminations = 10;
+
+
+    }   
+    
+    
+
+        
+    };
+    
+     
