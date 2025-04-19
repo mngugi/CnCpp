c662f60ee185571b61fc7773db4ac6a8999508ba
mngugi <mngugi7@gmail.com>
2025-04-19 16:48:03 +0300

    Update classes.cpp

---
+++ classes.cpp
@@ -0,0 +1,24 @@
+
+/***
+ * In the text above, the words “class” and “object” are used in an almost
+ * interchangeable manner. There are many similarities between classes and
+ * objects, but there is also an important difference. Working with objects has
+ * a lot of specialized vocabulary
+ * Classes - Classes are a collection of data and the actions that can modify the data. 
+ * Objects - Objects are constructed according to the blueprint that is the class.
+ * Instance - Another way that programmers talk about objects is to say that
+ * an object is an instance of a particular class. For example, s is an instance
+ * of the string class.
+ * Instantiation - Instantiation is the process where an object is created
+ * according to a blueprint of the class. The phrase “define a variable” means
+ * to create a variable. The variable is given a name and a value. Once it has
+ * been defined, you can use the variable. With objects, you use the phrase
+ * “instantiate an object”. That means to create an object, give it a name, store
+ * any data, and define the actions the object can perform.
+ */
+
+ #include <iostream>
+ #include<string>
+
+ using namespace std;
+
