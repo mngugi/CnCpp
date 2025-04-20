cd287d9a981d799b86f9d259d34f456576298a5f
mngugi <mngugi7@gmail.com>
2025-04-20 14:12:10 +0300

    Update mutability.cpp

---
+++ mutability.cpp
@@ -0,0 +1,19 @@
+
+/**
+ * Objects are mutable, which means that objects (specifically their attributes)
+ * can change value. Think of a video game; the main character in the game is
+ * constantly changing.
+ */
+
+ #include <iostream>
+ #include <string>
+
+ using namespace std;
+
+ class Player {
+    public:
+    int health;
+    int score;
+    int level;
+        
+ };
