af3e2572cd9140d37f033254df65cc3981d2683b
mngugi <mngugi7@gmail.com>
2025-04-20 12:55:36 +0300

    Create objects.cpp

---
+++ objects.cpp
@@ -0,0 +1,11 @@
+
+/***
+ * @ClassName 
+ * 1. All objects are created inside classes. The first step is use the keyword class.
+ * 2. Give the class a name which is used to construct an object.
+ * 3. The keyword public: is needed in order to provide access to object attributes that
+ *  you define later on.
+ * 4. Give your object as many attributes as needed in order to make full use of your object.
+ * 5. All attributes go within curly braces {} after the class name declaration.
+ * Make sure to end the closing curly brace with a semi-colon ;.
+ */
