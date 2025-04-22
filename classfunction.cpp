0bc9cbd3b6cb473e43324e713b26132dfbb13a52
mngugi <mngugi7@gmail.com>
2025-04-22 19:11:59 +0300

    Update classfunction.cpp

--- classfunction.cpp
+++ classfunction.cpp
@@ -1,4 +1,16 @@
 /**
  * a class was defined as “a collection of data and the actions that can modify the data.”
- * 
+ * using external functions to modify objects is not a good practice.
+ * class functions, also known as class or instance methods, that serve to modify the 
+ * data within objects. 
+ * Think of a class function as a function that is attached to an object. 
+ * The class function is the most common type of function when creating classes.
+ * Notice how class functions are declared inside of the class. These functions
+ * are called class functions because they have access to the class variables
+ * (the attributes declared in the constructor). Class functions are invoked
+ * using dot-notation.
  */
+
+ #include <iostream>
+ #include <string>
+ using namespace std;
