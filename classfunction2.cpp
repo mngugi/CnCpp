03290438f54f2e30f4e96717851f0e1bdaacacab
mngugi <mngugi7@gmail.com>
2025-04-22 19:41:19 +0300

    Create classfunction2.cpp

---
+++ classfunction2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+ #include <string>
+ using namespace std;
+
+ class Meal() {
+    private:
+        vector < string> drinks;
+        vector < string> appetizers;
+        vector < string> mainCourses;
+        vector < string> desserts;
+        vector < string> sides;
+        vector < string> salads;
+        vector < string> soups;
+        vector < string> breads;
+        vector < string> sauces;
+        vector < string> garnishes;
+        vector < string> condiments;            
+ }
