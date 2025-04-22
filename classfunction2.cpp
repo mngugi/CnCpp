a442e51168459065aded0fa6a4b7f12f19d1cfb4
mngugi <mngugi7@gmail.com>
2025-04-22 19:42:52 +0300

    Update classfunction2.cpp

--- classfunction2.cpp
+++ classfunction2.cpp
@@ -2,6 +2,16 @@
  #include <string>
  using namespace std;
 
+ /**
+  * Changes to objects should happen exclusively through class functions. This
+  * makes your code easier to organize and easier for others to understand.
+  * Imagine you are going to create a class that keeps track of a meal. In this 
+  * case, a meal can be thought of as all of the drinks, appetizers, courses, and
+  * desserts served. Each one of these categories will become a class variable
+  * (attribute). Assign each attribute a vector of strings. Remember, class
+  * variables/attribute are private.
+  */
+
  class Meal() {
     private:
         vector < string> drinks;
