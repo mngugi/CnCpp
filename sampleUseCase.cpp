a962141fdc53283671d8e052a78983e1038ec1c8
mngugi <mngugi7@gmail.com>
2025-04-23 00:12:13 +0300

    Update sampleUseCase.cpp

--- sampleUseCase.cpp
+++ sampleUseCase.cpp
@@ -37,7 +37,7 @@ int main() {
     vector<Student> students;
 
     students.push_back(Student("Bruno", 852369, "Data Science", 3.7, 35, 2028));
-    students.push_back(Student("Alice", 123456, "Cyber Security", 3.9, 22, 2026));
+    students.push_back(Student("Alice", 984556, "Cyber Security", 3.9, 22, 2026));
     students.push_back(Student("Bob", 654321, "AI", 3.6, 24, 2027));
 
     for (const Student& s : students) {
