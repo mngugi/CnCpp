b25b0987f9ce792d3445e6739fd0f1c23ea89001
mngugi <mngugi7@gmail.com>
2025-04-23 19:45:11 +0300

    breakForLoop.cpp

--- breakForLoop.cpp
+++ breakForLoop.cpp
@@ -9,6 +9,11 @@
  int main(){
 
     for (int i = 1; i <= 5; i ++){
-        
+        if (i == 2){
+            break; // break the loop when i is 2
+        }
+        cout << i << endl; // print the value of i
     }
+
+    return 0;
  }
