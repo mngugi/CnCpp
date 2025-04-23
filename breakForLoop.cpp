8b8bef1143d890e4dae50322516ae44245173dbf
mngugi <mngugi7@gmail.com>
2025-04-23 19:47:08 +0300

    Update breakForLoop.cpp

--- breakForLoop.cpp
+++ breakForLoop.cpp
@@ -5,11 +5,15 @@
 
  #include <iostream>
  using namespace std;
-
+/**
+ * This means, when i is equal to 3,
+ *  the break statement terminates the loop. 
+ * Hence, the output doesn't include values greater than or equal to 3.
+ */
  int main(){
 
     for (int i = 1; i <= 5; i ++){
-        if (i == 2){
+        if (i == 3){
             break; // break the loop when i is 2
         }
         cout << i << endl; // print the value of i
