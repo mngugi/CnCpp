ed51bed03f04f08ac2d8b92b1a98ad9fe43d0faa
mngugi <mngugi7@gmail.com>
2025-04-24 14:22:34 +0300

    Update arrsyAccess.cpp

--- arrsyAccess.cpp
+++ arrsyAccess.cpp
@@ -7,7 +7,7 @@ int main(){
     string names[5] = {"Allan", "Bob", "Charlie", "David", "Edward"};
    // int scores[5] = {71, 42, 99, 85, 60};
 
-    cout << "Allan: " << names[0] << endl;
+    cout << "Allan: " << names[0] << endl; // accessing the first element
 
     return 0;
 }
