d5aaf3f4a3983f0328459d1121bc9df973df14af
mngugi <mngugi7@gmail.com>
2025-04-24 14:11:49 +0300

    Update arrayVector2.cpp

---
+++ arrayVector2.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int Allan = 71;
+int Bob = 42;
+int Charlie = 99;
+int David = 85;
+int Edward = 60;
+int Frank = 78;
+int George = 90;
+int Henry = 55;
+int Ian = 88;
+
+cout << "Allan: " << Allan << endl;
+cout << "Bob: " << Bob << endl;
+cout << "Charlie: " << Charlie << endl;
+cout << "David: " << David << endl; 
+cout << "Edward: " << Edward << endl;
+cout << "Frank: " << Frank << endl;
+cout << "George: " << George << endl;
+cout << "Henry: " << Henry << endl;
+cout << "Ian: " << Ian << endl;
+cout << "The average score is: " << (Allan + Bob + Charlie + David + Edward + Frank + George + Henry + Ian) / 9 << endl;
+
+return
