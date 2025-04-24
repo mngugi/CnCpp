9c01e6a8520aee8ca24ffacf2c49816eabba0e69
mngugi <mngugi7@gmail.com>
2025-04-24 16:35:24 +0300

    Create arrayKeyPoints.cpp

---
+++ arrayKeyPoints.cpp
@@ -0,0 +1,25 @@
+
+/****
+ * @file arrayKeyPoints.cpp
+ * @brief This program demonstrates how to access elements in an array.
+ * @details
+ * 1. The program defines an array of strings called bools with 3 elements.
+ * 2. 
+ * 
+ */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int integers[1];
+
+bool bools[3] = {true, false, true};
+double doubles[3] = {1.1, 2.2, 3.3};
+
+int main(){
+
+    cout << bools[0] << endl; // accessing the first element
+    
+return 0;
+}
