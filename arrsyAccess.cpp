cc95e8cb4b8f8baaf883585adfdf5030dcedcdd1
mngugi <mngugi7@gmail.com>
2025-04-24 14:18:46 +0300

    arrsyAccess.cpp

--- arrsyAccess.cpp
+++ arrsyAccess.cpp
@@ -5,9 +5,9 @@ using namespace std;
 int main(){
 
     string names[5] = {"Allan", "Bob", "Charlie", "David", "Edward"};
-    int scores[5] = {71, 42, 99, 85, 60};
+   // int scores[5] = {71, 42, 99, 85, 60};
 
-    cout << "Allan: " << Allan << endl;
+    cout << "Allan: " << names[0] << endl;
 
-    retrun 0 ;
+    return 0;
 }
