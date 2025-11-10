c2833db6b5ddd7523c20ecd83ef7cba326dcc243
mngugi <mngugi7@hotmail.com>
2025-11-10 12:20:22 +0300

    Update print_fflush.c

--- print_fflush.c
+++ print_fflush.c
@@ -8,7 +8,16 @@ void handle_sigterm(int sig) {
     printf("SIGERM".\n);
     fflush(stdout);
 
+}
 
+int main() {
 
+    signal(SIGTERM, handle_sigterm);
+    printf("Am Process %d. Kill me uick!\n", getpid());
+
+    while(1) {
+        pause();
+    }
+    return 0;
 }
 
